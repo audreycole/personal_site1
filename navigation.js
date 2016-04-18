48f0e98bd71dc12f75f50f6f17f6f118c804bd13
Audrey Cole <audreycole88@yahoo.com>
2016-04-17 21:57:06 -0500

    added some words

--- navigation.js
+++ navigation.js
@@ -3,38 +3,18 @@ $(document).ready(function() {
     // jQuery methods go here...
 
 	$('#home_').click(function() {
-		$('html,body').animate({scrollTop: 0}, 1500);
+		$('html,body').animate({scrollTop: -11}, 1500);
 	});
 
+	var prevAboutOffset = 0;
 	$('#about_').click(function() {
-		var offset = $("#about").offset().top - 150;
-		console.log(offset);
-		$('html,body').animate({scrollTop: offset}, 1500);
+		$('html,body').animate({scrollTop: $("#about").offset().top}, 1500);
 	});
 
 	$('#contact_').click(function() {
 		$('html,body').animate({scrollTop: $("#contact").offset().top}, 1500);
 	});
 
-    // init variable for last scroll position
-	var lastScrollPosition = 0;
-
-	/*$(window).bind('scroll', function () {
-
-		var offset = $('#header').offset(); 
-
-		console.log(offset); 
-
-		// If you're scrolling down
-		if(currentScrollPosition > offset.top + 61) {
-			//$('.header clearfix').animate({bottom: "+=100px"});
-			$('#header').css({"position":"fixed","top":"0"});
-		} 
-		else {
-			$('#header').css({"position":"static","top":"auto"});
-		}
-	
-	}); */
 
 	$(document).scroll(function () {
         var scroll = $(this).scrollTop();
